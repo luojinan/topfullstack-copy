e0c9ba9d226968bcdbddfe47d9a28888eef6dc99
LuoJinAn <1017386624@qq.com>
2020-02-09 00:06:25 +0800

    用户数据库表添加创建时间、修改时间字段

--- server/libs/db/src/models/user.model.ts
+++ server/libs/db/src/models/user.model.ts
@@ -1,5 +1,12 @@
-import { prop } from '@typegoose/typegoose'
+import { prop, modelOptions } from '@typegoose/typegoose'
 import { ApiProperty } from '@nestjs/swagger'
+
+// 数据库表创建时间更新时间字段（参考原生mongoose）
+@modelOptions({
+  schemaOptions:{
+    timestamps:true
+  }
+})
 
 export class User{
   @ApiProperty({description:'用户名',example:'用户名1'})
