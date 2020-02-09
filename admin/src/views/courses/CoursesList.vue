90b1db95b2cb47d91516113255c9cf39a14f3cad
LuoJinAn <1017386624@qq.com>
2020-02-10 00:20:51 +0800

    引入avue依赖，简化html，ts数据报错待解决

--- admin/src/views/courses/CoursesList.vue
+++ admin/src/views/courses/CoursesList.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <h3>课程列表</h3>
+    <avue-crud 
+      :data="data" 
+      :option="option"
+      @row-save="add"
+      @row-update="edit"
+      @row-del="remove"
+      ></avue-crud>
   </div>
 </template>
 
@@ -11,11 +17,45 @@ import Component from 'vue-class-component'
 
 @Component({})
 export default class Main extends Vue{
-  data = {}
-  
+  data = []
+  option={
+    title:'课程管理',
+    // page:false,
+    align:'center',
+    menuAlign:'center',
+    column:[
+      {label:'课程名称',prop:'name'},
+      {label:'课程封面图',prop:'cover'}
+    ]
+  }
+
+  async add(form,done){
+    console.log(form)
+    await this.$axios.post('/apis/courses',form)
+    this.$message.success('新增成功')
+    this.fetch()
+    done()
+  }
+  async edit(form,index,done){
+    const data = {
+      id:form._id,
+      name:form.name,
+      cover:form.cover
+    }
+    await this.$axios.put(`/apis/courses`,data)
+    this.$message.success('更新成功')
+    this.fetch()
+    done()
+  }
+  async remove(form){
+    await this.$axios.delete(`/apis/courses`,{id:form._id})
+    this.$message.success('删除成功')
+    this.fetch()
+  }
   async fetch(){
     const res = await this.$axios.get('/apis/courses')
     console.log(res)
+    this.data = res
   }
 
   created () {
