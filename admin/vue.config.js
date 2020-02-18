f926b0e10d157a280fb1ef88bebf7711df1ebe6a
LuoJinAn <1017386624@qq.com>
2020-02-18 20:08:39 +0800

    测试上线

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -4,7 +4,8 @@ module.exports = {
     port: '',
     proxy: {
       '/apis': {
-        target: 'http://localhost:3000/', // target host
+        // target: 'http://localhost:3000/', // target host
+        target: 'http://134.175.187.48:3000/', // target host
         ws: true, // proxy websockets 
         changeOrigin: true, // needed for virtual hosted sites
         pathRewrite: {
--- server/libs/db/src/db.module.ts
+++ server/libs/db/src/db.module.ts
@@ -11,7 +11,7 @@ const dbModels = TypegooseModule.forFeature([User,Course,Episode])
 @Global()
 @Module({
   imports:[
-    TypegooseModule.forRoot('mongodb://134.175.187.48:27017/topfullstack',{
+    TypegooseModule.forRoot('mongodb://127.0.0.1:27017/topfullstack',{
       useNewUrlParser:true,
       useUnifiedTopology:true,
       useCreateIndex:true,
